841136373d9d95caf5d2422a99c4c48fe1288720
sophiev17 <sophiev@unc.edu>
2025-04-09 00:20:19 -0400

    EX04 Comment updates

--- exercises/ex04/bear.py
+++ exercises/ex04/bear.py
@@ -8,15 +8,18 @@ class Bear:
     hunger_score: int
 
     def __init__(self):
+        """age and hunger score of all bears start at 0"""
         self.age = 0
         self.hunger_score = 0
         return None
 
     def one_day(self):
+        """age inc and hunger score dec each day (lower score == more hungry)"""
         self.age += 1
         self.hunger_score -= 1
         return None
 
     def eat(self, num_fish: int) -> None:
+        """hunger score inc with more fish (bears are less hungry with more food)"""
         self.hunger_score += num_fish
         return None
